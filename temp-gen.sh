46716732c4907e1cbe4dcf82c7c02667f768c50e
Andrea Pallotta <andreapallotta9@gmail.com>
2022-07-16 12:31:58 -0400

    Create temp-gen.sh

---
+++ temp-gen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Check OS version
+
+# Check if subversion is installed and install if not
+
+# If package cannot be installed, exit with error message
+
+# If no flag is passed, Require user input on what template to pull and where to save it
+
+# Pull template
+
+exit 0
+
