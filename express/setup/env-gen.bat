fa95511f4057a37be1e2928d2efb11e0546e280b
AndreaPallotta <andreapallotta9@gmail.com>
2022-08-01 14:26:37 -0400

    Generate JWT token from batch

--- express/setup/env-gen.bat
+++ express/setup/env-gen.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET "file=.\.env"
+SET "file=..\.env"
 
 CALL :sub %file%
 EXIT /b
@@ -8,15 +8,17 @@ EXIT /b
 :sub
 SET "file_name=%~nx1"
 
+FOR /F "tokens=*" %%g IN ('node -e "console.log(require('crypto').randomBytes(256).toString('base64'));"') DO (SET secret=%%g)
+
 IF EXIST %file% (
-    ECHO %file_name% already present. Skipping to the next step...
+    ECHO %file_name% already present. Skipping...
 ) else (
     ECHO %file_name% does not exist. Creating it...
     (
         ECHO PORT=8081
         ECHO HOST="*INSERT HOST IP/NAME OR DELETE IF LOCALHOST*"
         ECHO LOG_LEVEL="*INSERT LOG LEVEL (error, warn, info, http, debug) OR DELETE FOR DEFAULT (debug for dev/warn for prod)"
-        ECHO SECRET="*INSERT JWT SECRET HERE*"
+        ECHO SECRET="%secret%"
     ) > %file%
     ECHO %file_name% created with default content.
 )
