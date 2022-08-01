88221fe00b47cafe6039bc494eb8e483ee5eab6a
AndreaPallotta <andreapallotta9@gmail.com>
2022-08-01 14:30:27 -0400

    create JWT token in bash. Fixed .env path

--- express/setup/env-gen.sh
+++ express/setup/env-gen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-file="./.env"
+file="../.env"
 file_name=${file##*/}
+
+secret=$(node -e "console.log(require('crypto').randomBytes(256).toString('base64'));")
 
 if [ -f "$file" ]; then
     echo "$file_name already present. Skipping to the next step..."
@@ -11,7 +13,7 @@ cat << EOT >> $file
 PORT=8081
 HOST="*INSERT HOST IP/NAME OR DELETE IF LOCALHOST*"
 LOG_LEVEL="*INSERT LOG LEVEL (error, warn, info, http, debug) OR DELETE FOR DEFAULT (debug for dev/warn for prod)"
-SECRET="*INSERT JWT SECRET HERE*"
+SECRET="$secret"
 EOT
     echo "$file_name created with default content."
 fi
