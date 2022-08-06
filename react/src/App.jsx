ef2baff1777d48a2fa100134ad67a6a955b9aca1
AndreaPallotta <andreapallotta9@gmail.com>
2022-08-06 04:05:24 -0400

    Added linting and notification snacks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,30 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { app } from './utils/env';
+import Notification from './components/Notification';
+import useNotification from './hooks/useNotification';
+import NotificationContext from './contexts/Notification';
+import ChildPage from './pages/ChildPage';
 
 function App() {
+  const [, close, notification, setNotification] = useNotification(true);
+
   useEffect(() => {
     document.title = app.name;
   }, []);
 
-  return <div className='App'></div>;
+  return (
+    <NotificationContext.Provider value={{ notification, setNotification }}>
+      <div className='App'>
+        <ChildPage />
+        <Notification
+          open={notification.open}
+          onClose={close}
+          message={notification.message}
+          severity={notification.severity}
+        />
+      </div>
+    </NotificationContext.Provider>
+  );
 }
 
 export default App;
---
+++ react/src/pages/ChildPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import useNotification from '../hooks/useNotification';
+
+const ChildPage = () => {
+  const [showNotification] = useNotification();
+  return (
+    <Button
+      onClick={() => {
+        showNotification('test3', 'success');
+      }}
+    >
+      Click Me Too
+    </Button>
+  );
+};
+
+export default ChildPage;
