0034869f02dc37c712801a93625a0f2f8eec1d19
AndreaPallotta <andreapallotta9@gmail.com>
2022-08-10 12:23:36 -0400

    Fixed jwt.js name type

--- express/endpoints/test/test.routes.js
+++ express/endpoints/test/test.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { validateToken } = require('@auth/jtw');
+const { validateToken } = require('@auth/jwt');
 const { testGet, testPost } = require('./test.controller');
 
 router.get('/test-get', validateToken, testGet);
