764a217780d7f8b7c7e88d4bb465d81a2c41b507
AndreaPallotta <andreapallotta9@gmail.com>
2022-08-13 01:08:24 -0400

    Added test validators

--- express/endpoints/test/test.validator.js
+++ express/endpoints/test/test.validator.js
@@ -1,2 +1,9 @@
-require('express-validator');
 const validate = require('@validation/validator');
+const { param, body } = require('express-validator');
+
+exports.validateGET = validate([param('username').not().isEmpty()]);
+
+exports.validatePOST = validate([
+    body('email', 'Invalid email').isEmail(),
+    body('password', 'Invalid password').isLength({ min: 8, max: 25 }),
+]);
