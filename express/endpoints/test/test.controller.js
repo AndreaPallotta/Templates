51874ac14cb27d31fc6bc0f59f1684b06f0f2c6c
AndreaPallotta <andreapallotta9@gmail.com>
2022-08-13 01:12:27 -0400

    modified test route to use validator

--- express/endpoints/test/test.controller.js
+++ express/endpoints/test/test.controller.js
@@ -1,32 +1,11 @@
-const Logger = require('@log/logger')
+const Logger = require('@log/logger');
 const HTTPError = require('@errors/HTTPError');
 
-exports.testGet = async (req, res) => {
-    const { queryParam1, queryParam2 } = req.query;
-
-    if (!queryParam1 && !queryParam1) {
-        Logger.error('Invalid params');
-        return HTTPError.Err(400, undefined, res);
-    }
-
-    if (queryParam1 === queryParam2) {
-        Logger.info(`params are equal: ${queryParam1}`);
-        return res.json('They are equal');
-    }
-
+exports.testGet = async ({ params }, res) => {
+    Logger.debug(`GET /test/test-get/ params ${params}`);
     return HTTPError.Err(404, 'Data not found', res);
 };
 
 exports.testPost = async (req, res) => {
-    const { bodyParam1, bodyParam2 } = req.body;
-
-    if (Object.keys(req.body).length === 0) {
-        return HTTPError.Err(400, undefined, res);
-    }
-
-    if (bodyParam1 === 'post request' && bodyParam2 === false) {
-        return HTTPError.Err(500, undefined, res);
-    }
-
     return res.json('Request completed');
 };
